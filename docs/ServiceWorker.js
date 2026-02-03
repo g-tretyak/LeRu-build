f21c690e4febe7bfb3eeea76c734876ff007f32d
g_tretyak <137423551+g-tretyak@users.noreply.github.com>
2026-02-03 23:50:44 +0300

    fix template

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-LeRu-v.01.03.02.2026.23:41";
+const cacheName = "DefaultCompany-LeRu-v.01.03.02.2026.23:50";
 const contentToCache = [
     "Build/docs.loader.js",
     "Build/docs.framework.js",
